0dee48cfc4e51bc8f3693ed83f6ca81bbfb80b30
Saeed Isa <saeed.isa90@gmail.com>
2017-10-15 18:31:11 +0300

    Initial commit

---
+++ app/src/main/java/com/androiddevelopments/saeedisa/safetransport/SafeTransportMainActivity.java
@@ -0,0 +1,13 @@
+package com.androiddevelopments.saeedisa.safetransport;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SafeTransportMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_safe_transport_main);
+    }
+}
